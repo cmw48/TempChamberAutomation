e644f2a79983f941dd8803af88da3d2e112e2bc5
Chris Westling <cmwestling@gmail.com>
2016-05-25 10:24:07 -0400

    testing oop

--- tempcontrolTEST01.py
+++ tempcontrolTEST01.py
@@ -33,18 +33,26 @@ deltas=[]
 global startblvrun
 
 class MQTT_Message:
-    def __init__(self, name):
-        self.values = []    # creates a new list of values for each message
 
-    def setmessage(self, msg_json):
-        self.values = msg_json
-        print('Hey, just loaded up the list.')
-        print(self.values)      
+    try:
+        def __init__(self, name):
+            self.values = []    # creates a new list of values for each message
+
+        def setmessage(self, msg_json):
+            self.values = msg_json
+            print('Hey, just loaded up the list.')
+            print(self.values)      
     
-    def getmessage(self, raw_instant_temp):
-        raw_instant_temp = (self.values['raw-instant-value'])    
-        return raw_instant_temp
+        def getmessage(self, raw_instant_temp):
+            raw_instant_temp = (self.values['raw-instant-value'])    
+            return raw_instant_temp
         
+    except IOError as e:
+      print "I/O error({0}): {1}".format(e.errno, e.strerror)
+    except ValueError:
+      print "Could not convert data to an integer."
+    except:
+      print "Unexpected error:", sys.exc_info()[0]
         
         
 # squawk when subscribe to egg topic is successful
