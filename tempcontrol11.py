9ffe1ccf862e3e9f31a2b518bd074f6c4437d82c
Chris Westling <cmwestling@gmail.com>
2016-06-08 16:14:36 -0400

    problem with arduino serial

--- tempcontrol11.py
+++ tempcontrol11.py
@@ -22,7 +22,7 @@ import getopt
 from pyfirmata import Arduino, util
 
 try:
-    board = Arduino('/dev/ttyACM0')
+    board = Arduino('/dev/tty1')
 except IOError as e:
     print "I/O error({0}): {1}".format(e.errno, e.strerror)
 except ValueError:
