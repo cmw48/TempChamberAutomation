ac84a5aa5f830c234cce7d686fb52a28c2aa2a0e
Chris Westling <cmwestling@gmail.com>
2016-07-10 23:40:25 -0400

    improving b version

--- testFirmatat.py
+++ testFirmatat.py
@@ -207,14 +207,17 @@ def main(argv):
 
     try:
         while 1 == 1:
+            print("one")
             board.digital[2].write(1)
             board.digital[3].write(1)
             board.digital[4].write(1)
             time.sleep(3)
+            print("two")
             board.digital[2].write(1)
             board.digital[3].write(0)
             board.digital[4].write(0)
             time.sleep(3)
+            print("three")
             board.digital[2].write(1)
             board.digital[3].write(1)
             board.digital[4].write(0)
