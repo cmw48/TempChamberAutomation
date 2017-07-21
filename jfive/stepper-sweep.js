9d237084c902c9487ac6049c214934d6a11d256d
cmw48 <chris@wickeddevice.com>
2017-07-21 15:23:06 +0000

    working on stepper motor code

--- jfive/stepper-sweep.js
+++ jfive/stepper-sweep.js
@@ -15,7 +15,7 @@ board.on("ready", function() {
   var stepper = new five.Stepper({
     type: five.Stepper.TYPE.DRIVER,
     stepsPerRev: 200,
-    pins: [11, 12]
+    pins: [5, 10]
   });
 
   stepper.rpm(180).ccw().step(2000, function() {
---
+++ jfive/steppertest.js
@@ -0,0 +1,15 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var k = 0;
+  var stepper = new five.Stepper({
+    type: five.Stepper.TYPE.DRIVER,
+    stepsPerRev: 200,
+    pins: [5, 10]
+  });
+
+  stepper.rpm(180).ccw().step(2000, function() {
+    console.log("done");
+  });
+});
